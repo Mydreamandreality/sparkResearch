3e85f61a83705dd7de749f18d3b8202a7e9744b2
helloworld <844255243@qq.com>
2019-03-21 13:42:33 +0800

    pairRDD操作

--- chapter6/src/main/java/pair/PairRdd.java
+++ chapter6/src/main/java/pair/PairRdd.java
@@ -4,12 +4,15 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
+import java.nio.channels.InterruptedByTimeoutException;
 import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Created by 張燿峰
@@ -101,5 +104,41 @@ public class PairRdd {
         JavaPairRDD<String, Tuple2<Iterable<String>, Iterable<String>>> groupRDD = pairRDD.cogroup(pairRDDOther);
 
 
+        //pairRDD也可以使用RDD的函数
+        //筛选length小于20的元素
+        Function<Tuple2<String, String>, Boolean> filterRDD = new Function<Tuple2<String, String>, Boolean>() {
+            @Override
+            public Boolean call(Tuple2<String, String> v1) {
+                return (v1._2.length() < 20);
+            }
+        };
+        JavaPairRDD<String, String> filter = pairRDD.filter(filterRDD);
+
+        JavaRDD<String> wordCount = sparkContext.parallelize(Arrays.asList("1", "2", "3", "4", "5"));
+
+        //返回一个可以迭代的集合
+        JavaRDD<String> c = wordCount.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterator<String> call(String v1) throws Exception {
+                return Arrays.asList(v1.split(" ")).iterator();
+            }
+        });
+
+        //现在的数据是 1,2,3,4,5
+        JavaPairRDD<String, Integer> result = c.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2<>(s, 1);
+                //此时的数据是 {1,1},{2,1},{3,1},{4,1}...
+            }
+        }).reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+
+        /*还可以通过countByValue快速实现单词计数*/
+        c.countByValue();
     }
 }
